3d311a93a72eea67f7e746ed2cfe4ff83d758873
Paul <marton.maps@gmail.com>
2021-09-28 07:09:02 -0300

    Tweaked a little the tilemap
    Added collision for buildings.
    
    Testing lights.

---
+++ scenes/Entities/Items/StaticObject/StaticObject.gd
@@ -0,0 +1,15 @@
+extends Node
+
+onready var n_sprite := $Sprite
+
+func _ready():
+	yield(get_tree().create_timer(.25),"timeout")
+	var poly = $CollisionShape.get_polygon()
+	$Area2D/CollisionShapeArea.set_polygon(poly)
+	$Area2D.position += Vector2.UP * 4
+
+func _on_body_entered(body):
+	n_sprite.self_modulate.a = .77
+
+func _on_body_exited(body):
+	n_sprite.self_modulate.a = 1.0
