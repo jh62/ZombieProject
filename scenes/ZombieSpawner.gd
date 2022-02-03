5a6b413c373839366f1977e07f5f226229f8b2c8
Paul <marton.maps@gmail.com>
2022-02-03 03:54:13 -0300

    Fixed visibility issue when spawning zombies
    Zombies should not longer spawn when on camera.

--- scenes/ZombieSpawner.gd
+++ scenes/ZombieSpawner.gd
@@ -17,7 +17,7 @@ var spawn_count := 0
 func _ready() -> void:
 	tilemap = get_parent()
 	if active:
-#		yield(get_tree().create_timer(.15),"timeout")
+		yield(get_tree().create_timer(1.0),"timeout")
 		_spawn_mob()
 
 func _process(delta: float) -> void:
@@ -43,16 +43,18 @@ func _process(delta: float) -> void:
 func _spawn_mob(count := randi() % mob_group_max + 1) -> void:
 	var areas := tilemap.get_node("AreaSpawns")
 	for area in areas.get_children():
+		yield(get_tree().create_timer(0.01),"timeout")
 		var area_pos = area.global_position
 		n_visible.global_position = area_pos
-		yield(get_tree().create_timer(0.18),"timeout")
+
 		if n_visible.is_on_screen(): # don't spawn zombies in player's view, it's not nice
-			print_debug("zombie visible")
+#			print_debug("zombie visible")
 			continue
 		for i in count:
 			var angle := rand_range(0.0, 2.0) * PI
 			var direction = Vector2(cos(angle), sin(angle))
 			var pos =  area_pos + direction * area.shape.radius
+
 			spawn_count += 1
 			EventBus.emit_signal("on_mob_spawn", pos)
 
